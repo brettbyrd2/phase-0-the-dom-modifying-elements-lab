8bc413b84b9c31450cd96b13cfbb8a2c09de2e60
Brett Byrd <byrdb2@yahoo.com>
2023-03-09 18:43:34 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeMain = document.querySelector('main')
+removeMain.parentElement.removeChild(removeMain)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'BRETT is the champion'
